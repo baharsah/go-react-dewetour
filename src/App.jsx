348252da8f8b7cad0ce059aca614017157445eb4
baharsah <me@baharsah.my.id>
2022-12-31 11:07:47 +0700

    create checker

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import React , {useContext} from 'react'
 import CustomNavbar from './components/navbar'
 import Footer from './components/Footer';
 import { BrowserRouter as Router, Routes ,Route } from 'react-router-dom';
@@ -7,6 +7,61 @@ import DetailTour from './components/DetailTour'
 import PaymentPending from './components/PaymentPending';
 import Profile from './components/Profile';
 import IncTrx from './components/IncTrx';
+import { UserContext } from './components/context/userProvider'; 
+import { useContext } from 'react'
+
+const [state, dispatch] = useContext(UserContext);
+
+
+useEffect(() => {
+  if (localStorage.token) {
+    setAuthToken(localStorage.token);
+  }
+
+  // Redirect Auth
+  if (state.isLogin === false) {
+    navigate('/auth');
+  } else {
+    if (state.user.status === 'admin') {
+      // navigate('/product-admin');
+    } else if (state.user.status === 'customer') {
+      navigate('/');
+    }
+  }
+}, [state]);
+
+
+const checkUser = async () => {
+  try {
+    const response = await API.get('/check-auth');
+
+    // If the token incorrect
+    if (response.status === 404) {
+      return dispatch({
+        type: 'AUTH_ERROR',
+      });
+    }
+
+    // Get user data
+    let payload = response.data.data.user;
+    // Get token from local storage
+    payload.token = localStorage.token;
+
+    // Send data to useContext
+    dispatch({
+      type: 'USER_SUCCESS',
+      payload,
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+useEffect(() => {
+  if (localStorage.token) {
+    checkUser();
+  }
+}, []);
 
 
 
